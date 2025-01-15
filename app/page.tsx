d8f8b7dcb2851959d4ef2599a96fbfaac9f76b68
Erik Svanes <svanes.erik@gmail.com>
2025-01-14 19:57:34 -0500

    try again

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,7 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
           <li>Oh I will :)</li>
+          <li>Let's try this again</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
